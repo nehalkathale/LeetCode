dc26302812492122d49426fe5699407ea0943b9a
nehalkathale <66513781+nehalkathale@users.noreply.github.com>
2022-01-07 18:34:40 -0500

    Time: 6 ms (66.92%), Space: 38.9 MB (89.48%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -3,7 +3,7 @@ class Solution {
         if (s.isEmpty()) {
             return 0;
         }    
-        int start = 0, end =0, max =0;
+        int start = 0, end = 0, max =0;
         HashSet<Character> set = new HashSet<Character>();
         while( end < s.length()){
             if(!(set.contains(s.charAt(end)))){
